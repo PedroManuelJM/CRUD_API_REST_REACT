f71fd70f4583e72321dae34a512020e23cac6ea0
Pedro Manuel Jurado Moreno <mpedrojurado@gmail.com>
2021-02-05 20:21:46 -0500

    ruta

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import reportWebVitals from './reportWebVitals';
 import Usuario from './Components/Usuario';
-
+import { HashRouter,Route, Switch} from "react-router-dom";
 ReactDOM.render(
+
   <React.StrictMode>
-   <Usuario/>
+    <HashRouter basename={process.env.PUBLIC_URL + "/"}>
+      <Switch>
+        <Route exact path="/" component={Usuario} />
+      </Switch>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
